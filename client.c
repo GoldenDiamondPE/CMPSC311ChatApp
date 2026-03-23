3878dae1ec5096157838e86feb745fcd34be93a7
AaronP1701 <aaronpoon100@gmail.com>
2026-03-22 23:33:54 -0400

    update
    updated the Server and client for basics, server is only able to send messages, client is only able to receive

--- client.c
+++ client.c
@@ -1,20 +1,113 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+# include <stdio.h>
+
+# include <stdlib.h>
+# include <unistd.h> //brings the close() command
+# include <sys/types.h> //helps initiate the following two packages
+# include <sys/socket.h> //1 creates socket
+# include <netinet/in.h> //2 contains structure, call def to know address and the port of remote socket
+
+# include <errno.h> 
+# include <string.h>
+
+/*
+At this time, the client is only able to recieve messages
+
+Set up for min requirments for server to be tested -> 
+feel free to add more, such as the GUI functions
+*/
+
+
+void functions(const char *messg1){ //testing if the server message can initiate functions
+  
+    //ideally the GUI function, or update function
+}
+
+int update(int response){
+// how to check if only a signal is received?
+    if ( response > -1){
+             // print out data we got back
+        printf("server data: %d\n", response); //prints the server_response - the char message.
+    }
+    return (-1);
+}
 
 int main(){
-    /*
-    SOCKET
-    */
-    // Socket File Desc
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0); //IPV4 TCP Protocol
 
-    // Sees if sockfd ran properly
-    if(sockfd ==-1){
-        printf("CLIENT: Error With Socket Creation\n");
-        return -1;
-    } else {
-        printf("CLIENT: Socket Creation Success\n");
-    }    
+    int serverFD  = socket(AF_INET, SOCK_STREAM, 0);
+    // server chooses (IPV4 addressing, TCP, default protocol(0))
+
+
+    //bind() bind to port - specifies the address for the socket
+
+    struct sockaddr_in server_address; // sockaddr_in is designed for IPv4
+
+    //defines address remotely
+    server_address.sin_family = AF_INET; // tells kernel to use IPv4 protocol
+    server_address.sin_port = htons(8080); //80 = HTTP - htons() converts address to appropriate data format for TCP
+    server_address.sin_addr.s_addr = INADDR_ANY; //This accesses the address itself .s_addr
+
+
+
+    int connection_status;
+    connection_status = connect(serverFD, (struct sockaddr *) &server_address, sizeof(server_address));
+    // connect() is a testing method, it returns an integer, -1,0
+
+    if (connection_status < 0){
+        perror("connection was not successful\n");
+        return(-1);
+    }
+    else{
+        printf("arrived!\n");
+    
+
+        
+        //recieves data from the server - this is usually after the listen portion
+        char server_response[268];// Your "bucket" - used to hold the data that is recieved from network
+       // char client_message[256]; //client's message to send back
+        char preNode[268] = "0_input";
+        int flag = 0;
+
+        while(1){
+        //only updates when client recieves data
+        // after each update, flag should be checked if it is the same as last output's
+        // the flag is indicated by server_response[0], ex: "1: Hello!""
+            ssize_t n = recv(serverFD, &server_response, sizeof(server_response) -1, 0);
+
+            //n = bytes recieved
+            if (n > 0){
+                server_response[n] = '\0'; //resets reponse 
+                printf("Server %d: %s\n", flag + 1, server_response);
+                flag++;
+
+            }else if (n == 0){
+                printf("Server closed connection.\n");
+                break;
+            }else{
+                perror("recv failed");
+                break;
+            }
+            /*
+            old code
+
+            // after the read, preNode should or should'nt equal message. 
+            if (server_response[0] != preNode[0] ){
+                printf("server data: %s\n", server_response);
+                // maybe call GUI update here
+                flag ++;
+                preNode[0] = server_response[0];
+
+            }
+                */
+
+
+        }
+
+        // close the socket
+        close(serverFD);
+    
+    }
+    //functions(server_response);
+    
+    return 0;
 }
+
--- server.c
+++ server.c
@@ -1,97 +1,114 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+# include <stdio.h>
+//# include "display.h"
+# include <stdlib.h>
+# include <sys/socket.h>
+# include <sys/types.h>
+# include <unistd.h>
+# include <netinet/in.h>
 #include <arpa/inet.h>
-#include <unistd.h>
+
+
+//At this time the server is only able to send messages
+
+#define MAX_CLIENTS 100 //defines the limit of clients
+int g_client_sockets[MAX_CLIENTS];
+int g_client_count = 0;
+
+void listOClients(const char *client){
+
+// client data would be stored in this list
+
+}
 
 int main(){
-    /*
-    SOCKET
-    */
-    // Socket File Desc
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0); //IPV4 TCP Protocol
 
-    // Sees if sockfd ran properly
-    if(sockfd ==-1){
-        printf("SERVER: Error With Socket Creation\n");
-        return -1;
-    } else {
-        printf("SERVER: Socket Creation Success\n");
-    }
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-    /*
-    BIND
-    */
-    struct sockaddr_in server_addr; // Creates server_addr struct
-    server_addr.sin_family = AF_INET; // Uses IPV4
-    server_addr.sin_port = htons(8080); // Sets SERVER Port
+    //defines server address
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8080);
 
     // Uses 127.0.0.1 as an IP Addr
-    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
         printf("SERVER: Invalid address\n");
         return -1;
     }
+    //server_address.sin_addr.s_addr = INADDR_ANY;//resolves to any IP address on local machine
 
-    // Calls Bind to see if it worked
-    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    //binding section
+    int binds = bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+   
+    if (binds< 0 ){
         printf("SERVER: Bind failed");
         return -1;
     } else {
         printf("SERVER: Bind successful\n");
-    }
+   }
+        
 
-    /*
-    LISTEN
-    */    
-    if (listen(sockfd, 10)== -1){
-        printf("SERVER: Listen failed\n");
+    //listen()
+    //listen (server_socket, 5);
+
+    if (listen(server_socket, 5) == -1){
+       printf("SERVER: Listen failed\n");
         return -1;
     } else {
-        printf("SERVER: Listen successful\n");
-    }
+        printf("SERVER: Listen successful\n"); 
+    };
+    
 
-    /*
-    ACCEPT
-    */
-    struct sockaddr_in client_addr;
-    socklen_t client_len = sizeof(client_addr);
-    int new_socket = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
+    //listens at this socket, any # besides 1 for small traffic levels, 
+    int client_socket;
 
-    if (new_socket < 0) {
+    //accept()
+
+    //define a client list of addresses
+    
+   /* if (client_socket < 0){ //testing
         perror("SERVER: Accept failed");
         return -1;
     } else {
         printf("SERVER: Accept successful! Client connected.\n");
+        //add the clients info to the list
+        //addClient(client_socket);
+       // printClientList();
     }
-
-    /*
-    RECV()
-    */
-    char buffer[1024]; // Your "bucket"
+       
+*/
+/*
+    char buffer[268]; // Your "bucket"
     ssize_t bytes_received = recv(new_socket, buffer, sizeof(buffer) - 1, 0);
 
     if (bytes_received > 0) {
-    buffer[bytes_received] = '\0'; // Add null terminator so printf knows where to stop
-    printf("Client says: %s\n", buffer);
+        buffer[bytes_received] = '\0'; // Add null terminator so printf knows where to stop
+        printf("Client says: %s\n", buffer);
     } else if (bytes_received == 0) {
         printf("Client disconnected.\n");
     } else {
         perror("recv failed");
     }
     
-    while (1) {
-        ssize_t bytes_received = recv(new_socket, buffer, sizeof(buffer) - 1, 0);
-        if (bytes_received <= 0) break; // Exit loop if error or disconnect
+    */
+    client_socket = accept(server_socket, NULL,NULL);
+    //send the message
+    int numbFlag = 1;
+    char server_message[256]; // message
+    char finalM[268]; // numbFlag + message
 
-        buffer[bytes_received] = '\0';
-        printf("Client: %s", buffer);
 
-        // Echo the message back to the client
-        send(new_socket, buffer, bytes_received, 0);
+    while(1){
+        scanf("%255s",server_message );//max char to be taken in
+        //snprintf(finalM, sizeof(finalM), "%d: %s", numbFlag, server_message);
+
+        send(client_socket, server_message, sizeof(server_message), 0);
+       
+       
     }
     
 
-    close(new_socket);
-    close(sockfd);  
-    printf("END\n");
+    close(server_socket);
+    close(client_socket);
+    return 0;
 }
+//bulbasaur
