ba77ce74e2152f1482d04082e01562ed20864a13
Tossac <alexander.hall100387@gmail.com>
2026-04-26 12:12:53 -0400

    updated fp_server.c
    Cleaned up comments/old code, formatting, minor changes.

--- fp_server.c
+++ fp_server.c
@@ -32,125 +32,91 @@ static void * threadrec(void *arg){
    
     printf("SERVER: Client connected on socket %d\n", client_socket);
 
-    //while (1) {
-		//first message received from client is entry of user name
-        ssize_t bytes_received = recv(client_socket, users[client_socket], sizeof(users[client_socket]) - 1, 0);
+	//first message received from client is entry of user name
+    ssize_t bytes_received = recv(client_socket, users[client_socket], sizeof(users[client_socket]) - 1, 0);
 
-		users[client_socket][bytes_received]='\0';
-		//diagnostic printing of username and socket. Can be deleted before final version
-		printf("%s\n",users[client_socket]);
-		printf("%d\n",client_socket);
+	users[client_socket][bytes_received]='\0';
+	
+	//diagnostic printing of username and socket. Can be deleted before final version
+	printf("%s connect on socket %d.\n",users[client_socket],client_socket);
 		
-		//Added by Matt - send join message to other clients
-		char join_message[268];
+	//Added by Matt - send join message to other clients
+	char join_message[268];
 		
-		pthread_mutex_lock(&client_mutex);
+	pthread_mutex_lock(&client_mutex);
 		
-		/*Build the join message*/
-		snprintf(join_message, sizeof(join_message), "*** %s has join the chat ***", users[client_socket]);
+	/*Build the join message*/
+	snprintf(join_message, sizeof(join_message), "*** %s has join the chat ***", users[client_socket]);
 		
-		/*Send to other clients*/
-		for(int i = 0; i < g_client_count; i++){
-			if(g_client_sockets[i] != client_socket){
+	/*Send to other clients*/
+	for(int i = 0; i < g_client_count; i++){
+		if(g_client_sockets[i] != client_socket){
+            send(g_client_sockets[i], join_message, strlen(join_message), 0);
+		}
+	}
+	pthread_mutex_unlock(&client_mutex);
+	//Added by Matt ^^^
+		
+    if (bytes_received > 0 ){
+		/* Broadcast message to all connected clients */
+		while((bytes_received = recv(client_socket, buffer, sizeof(buffer) - 1, 0)) > 0 ){
+			pthread_mutex_lock(&client_mutex);
+
+            snprintf(whole_message, sizeof(whole_message), "%s: %s", users[client_socket], buffer);
+            for (int i = 0; i < g_client_count; i++) {
+                /* Don't send message back to sender*/
+                if ((g_client_sockets[i] != client_socket) && (buffer[0] != '\0')){
+                    send(g_client_sockets[i], whole_message, strlen(whole_message), 0);
+                }
+             }
+				
+             //clear buffers
+			memset(&buffer, 0, sizeof(buffer));
+			memset(&whole_message, 0, sizeof(whole_message));
+            pthread_mutex_unlock(&client_mutex);
+        }
+          
+	}
+	if (bytes_received == 0) {
+        printf("SERVER: Client on socket %d disconnected\n", client_socket);
                 
-				send(g_client_sockets[i], join_message, strlen(join_message), 0);
+        //Added by Matt - send disconnect message to other clients
+        char leave_message[268];
+        pthread_mutex_lock(&client_mutex);
+                
+        snprintf(leave_message, sizeof(leave_message), "*** %s has left the chat ***", users[client_socket]);
+                
+        for(int i = 0; i < g_client_count; i++){
+			if(g_client_sockets[i] != client_socket){
+				send(g_client_sockets[i], leave_message, strlen(leave_message), 0);
 			}
 		}
-		
-        
-		pthread_mutex_unlock(&client_mutex);
-		//Added by Matt ^^^
-		
-        if (bytes_received > 0 ){
-				/*buffer hasn't been touched yet. will reset at bottom of following 'while' loop
-                buffer[bytes_received] = '\0'; //resets reponse 
-                printf("CLIENT[%d]: %s\n", client_socket, buffer);
-                /* OLD: sends message back only to same client
-				send(client_socket, server_message, strlen(server_message), 0);
-				*/
-
-				/* NEW: Broadcast message to all connected clients */
-			while((bytes_received = recv(client_socket, buffer, sizeof(buffer) - 1, 0)) > 0 ){
-				pthread_mutex_lock(&client_mutex);
-
-                
-                for (int i = 0; i < g_client_count; i++) {
-                    /* Don't send message back to sender*/
-                    if (g_client_sockets[i] != client_socket) {
-                    snprintf(whole_message, sizeof(whole_message), "%s: %s", users[client_socket], buffer);
-
-                        if (buffer[0] != '\0'){
-                        send(g_client_sockets[i], whole_message, strlen(whole_message), 0);
-                    }
-                    }
-                }
-                //clear buffers
-                for(int i=0; i<sizeof(buffer); i++){
-                    buffer[i] = '\0';
-                }
-                for(int i=0; i<sizeof(whole_message); i++){
-                    whole_message[i] = '\0';
-                }
-                pthread_mutex_unlock(&client_mutex);
-            }
-          
-		}
-		if (bytes_received == 0) {
-                printf("SERVER: Client on socket %d disconnected\n", client_socket);
-                
-                //Added by Matt - send disconnect message to other clients
-                char leave_message[268];
-                pthread_mutex_lock(&client_mutex);
-                
-                snprintf(leave_message, sizeof(leave_message), "*** %s has left the chat ***", users[client_socket]);
-                
-                for(int i = 0; i < g_client_count; i++){
-					if(g_client_sockets[i] != client_socket){
-						send(g_client_sockets[i], leave_message, strlen(leave_message), 0);
-					}
-				}
 				
-				pthread_mutex_unlock(&client_mutex);
-        }
-        else{ //if (bytes_received < 0){
-                 perror("SERVER: recv failed");
-		}
-                //break;
-            //}
-    //}
-
-    /*close(client_socket);
-    return NULL;*/
-    
+		pthread_mutex_unlock(&client_mutex);
+    }
+    else{ //if (bytes_received < 0){
+        perror("SERVER: recv failed");
+	}
+                
     //Matt: Remove disconnected client from list
     pthread_mutex_lock(&client_mutex);
-
 	for (int i = 0; i < g_client_count; i++) {
-
     	if (g_client_sockets[i] == client_socket) {
-
         	for (int j = i; j < g_client_count - 1; j++) {
-
             	g_client_sockets[j] = g_client_sockets[j + 1];
-
         	}
 
-        	g_client_count--;
-        	break;
+        g_client_count--;
+        break;
     	}
 	}
-
 	pthread_mutex_unlock(&client_mutex);
     //^^Matt^^
 }
 
 
 
-
-
-
 int main(){
-
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     //defines server address
@@ -163,7 +129,6 @@ int main(){
         printf("SERVER: Invalid address\n");
         return -1;
     }
-    //server_address.sin_addr.s_addr = INADDR_ANY;//resolves to any IP address on local machine
 
     //binding section
     int binds = bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
@@ -171,55 +136,21 @@ int main(){
     if (binds< 0 ){
         perror("SERVER: Bind failed");
         return -1;
-    } else {
+    }
+	else {
         printf("SERVER: Bind successful\n");
-   }
-        
-
-    //listen()
-    //listen (server_socket, 5);
+   	}
 
     if (listen(server_socket, 5) == -1){
        printf("SERVER: Listen failed\n");
         return -1;
-    } else {
+    }
+	else {
         printf("SERVER: Listen successful\n"); 
-    };
+    }
     
-
     //listens at this socket, any # besides 1 for small traffic levels,
     int client_socket;
-
-    //accept()
-
-    //define a client list of addresses
-    
-   /* if (client_socket < 0){ //testing
-        perror("SERVER: Accept failed");
-        return -1;
-    } else {
-        printf("SERVER: Accept successful! Client connected.\n");
-        //add the clients info to the list
-        //addClient(client_socket);
-       // printClientList();
-    }
-       
-*/
-/*
-    char buffer[268]; // Your "bucket"
-    ssize_t bytes_received = recv(new_socket, buffer, sizeof(buffer) - 1, 0);
-
-    if (bytes_received > 0) {
-        buffer[bytes_received] = '\0'; // Add null terminator so printf knows where to stop
-        printf("Client says: %s\n", buffer);
-    } else if (bytes_received == 0) {
-        printf("Client disconnected.\n");
-    } else {
-        perror("recv failed");
-    }
-    
-    */
-
 
     while(1){
         client_socket = accept(server_socket, NULL, NULL);
@@ -234,11 +165,9 @@ int main(){
             close(client_socket);
             continue;
         }
-       
-
+		
         *client_arg = client_socket;
 
-        
         //Matt: Adds client socket to global list
         pthread_mutex_lock(&client_mutex);
 
@@ -261,7 +190,6 @@ int main(){
        
 
         pthread_detach(t); // used for cleanup
-        /*g_client_count++; - I think this may not be needed since it's already been done within the if statement above? - Matt*/
     }
     
 
